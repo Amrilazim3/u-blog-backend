9a8f6ad7299a99564eec0cd288c2674d372a5405
Amrilazim3 <a.azim0711@gmail.com>
2022-11-26 00:38:01 +0800

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+}
