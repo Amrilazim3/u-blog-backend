ae503aead1f353a6991db4078a716633ea230567
Amrilazim3 <a.azim0711@gmail.com>
2022-12-01 09:28:32 +0800

    create route group for auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
