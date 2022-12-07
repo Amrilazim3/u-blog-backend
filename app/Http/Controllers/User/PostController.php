3e0ba69c65f31006142e8653d2a1286972d23b28
Amrilazim3 <a.azim0711@gmail.com>
2022-12-07 14:43:39 +0800

    create User folder to store all controller related with other user

---
+++ app/Http/Controllers/User/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(User $user)
+    {
+        $posts = Post::where('user_id', $user->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
+
+        return response()->json([
+            'posts' => $posts,
+            'user' => $user
+        ]);
+    }
+
+    public function show(User $user, Post $post)
+    {
+        $rPost = Post::with('user')->find($post->id);
+
+        return response()->json([
+            'post' => $rPost
+        ]);
+    }
+}
