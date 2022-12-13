248f3628ee7d5d978cb0ff7377cec9e6c604c6d7
Amrilazim3 <a.azim0711@gmail.com>
2022-12-13 14:02:20 +0800

    add relationship

--- app/Models/Engagement.php
+++ app/Models/Engagement.php
@@ -10,4 +10,14 @@ class Engagement extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function following()
+    {
+        return $this->belongsTo(User::class, 'engaged_id', 'id');
+    }
 }
